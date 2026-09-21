59c3d6991d6f3f74ad18af7d5ad895d701d3068c
Abi-coder007 <abishekcoder2007@gmail.com>
2026-09-21 20:55:04 +0530

    Add files via upload

---
+++ creating table2.sql
@@ -0,0 +1,19 @@
+CREATE TABLE list(
+name VARCHAR,
+sec CHAR,
+roll INT,
+atendance BOOL
+);
+
+
+SELECT * FROM list ; 
+
+INSERT INTO list(
+name,sec,roll,atendance
+)
+values
+(
+'abiiiii','A',01,TRUE
+);
+
+SELECT * FROM list;
